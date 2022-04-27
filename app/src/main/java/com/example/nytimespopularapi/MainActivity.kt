8fb149ccc45199cbd01f5ee26d0383511495d033
ibrahim <hamza.sdsol@gmail.com>
2022-04-27 11:12:58 +0500

    Initial Commit

---
+++ app/src/main/java/com/example/nytimespopularapi/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.nytimespopularapi
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
